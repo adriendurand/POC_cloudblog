8b9dc06e4fa1a1eb8cd62db1b17ee76d9ca679b1
Adrien DURAND <adurand.contact@gmail.com>
2017-12-04 11:46:03 +0100

    Add external configuration and improving logger

--- src/main/java/fr/adurand/poc/cloudblog/config/MongoConfig.java
+++ src/main/java/fr/adurand/poc/cloudblog/config/MongoConfig.java
@@ -2,20 +2,30 @@ package fr.adurand.poc.cloudblog.config;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
+@Data
 @Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties("spring.data.mongodb")
 public class MongoConfig extends AbstractMongoConfiguration {
+
+    private String ip;
+    private int port;
+    private String db;
 
     @Override
     protected String getDatabaseName() {
-        return "cloudblog";
+        return db;
     }
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient("172.19.0.2", 27017);
+        return new MongoClient(ip, port);
     }
 
 }
